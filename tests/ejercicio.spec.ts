52758171380d3dd3c708e4e4706209014539f38b
Nicolas Vegas <alu0101321745@ull.edu.es>
2022-04-20 23:27:01 +0000

    Configurando repositorio

--- tests/ejercicio.spec.ts
+++ tests/ejercicio.spec.ts
@@ -1,5 +1,9 @@
 import 'mocha';
 import {expect} from 'chai';
+import {add} from '../src/index';
 
 describe('Pruebas del ejercicio:', () => {
+    it('Pruebas add: ', () => {
+        expect(add(1, 2)).to.be.eql(3);
+    });
 });
