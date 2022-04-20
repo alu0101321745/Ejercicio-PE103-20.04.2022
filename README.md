f5c03e33bf3bd41aa68cf87b7e5e842c816fb244
Nicolas Vegas <alu0101321745@ull.edu.es>
2022-04-20 13:36:17 +0000

    Configurando repositorio

---
+++ README.md
@@ -0,0 +1,6 @@
+# Ejercicios PE103 
+##### Fecha: 20 de Abril de 2022
+
+
+## Inicio
+
---
+++ tests/ejercicio.spec.ts
@@ -0,0 +1,5 @@
+import 'mocha';
+import {expect} from 'chai';
+
+describe('Pruebas del ejercicio:', () => {
+});
